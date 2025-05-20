afa49426fe560778c992a3bf42f348a0ee8132c1
khueenaa <queenacalistarimiela@gmail.com>
2025-05-20 08:11:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Welcome to Latihan Informatika")
+st.title("Helloo!🧏🏻‍♀️")
 st.write(
-    "Let's start building the web!"
+    "Roses are red, violets are blue, sugar is sweet, And so are you!"
 )
 st.image("db24efc41ab7fd7fe32beb77d7271143-removebg-preview.png", width=200)
