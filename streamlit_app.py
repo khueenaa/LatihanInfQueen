d9f848d10fd4726799bd05b0ba06dcd46d962066
khueenaa <queenacalistarimiela@gmail.com>
2025-05-20 08:01:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Welcome to Latihan Informatika")
 st.write(
-    "Let's start building!"
+    "Let's start building the web!"
 )
 st.image("db24efc41ab7fd7fe32beb77d7271143-removebg-preview.png", width=200)
